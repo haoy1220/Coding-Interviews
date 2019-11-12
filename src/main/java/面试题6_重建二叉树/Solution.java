48831b3cfb3f568b9d53ebde48c597fa8c29764e
unknown <haoaying@outlook.com>
2019-11-12 10:01:25 +0800

    面试题7 用两个栈实现队列

--- src/main/java/面试题6_重建二叉树/Solution.java
+++ src/main/java/面试题6_重建二叉树/Solution.java
@@ -8,6 +8,19 @@ import java.util.Arrays;
  * @author Hao
  * @date 2019/11/11 22:49
  */
+
+/*
+ * 题目描述
+ * 输入某二叉树的前序遍历和中序遍历的结果，请重建出该二叉树。假设输入的前序遍历和中序遍历的结果中都不含重复的数字。
+ * 例如输入前序遍历序列{1,2,4,7,3,5,6,8}和中序遍历序列{4,7,2,1,5,3,8,6}，则重建二叉树并返回。
+ */
+
+/*
+ * 思路
+ * 1.前序遍历第一个为根节点，中序遍历根节点两边分别为左右子节点；
+ * 2.由1可以看出，每次找到根节点后可以将数组划分为左右节点对应的前序遍历和中续遍历；
+ * 3.之后都是重复操作，故可以考虑使用递归解决问题。
+ */
 public class Solution {
     public TreeNode reConstructBinaryTree(int[] pre, int[] in) {
         TreeNode root = Construct(pre, 0, pre.length - 1, in, 0, in.length - 1);
@@ -31,3 +44,24 @@ public class Solution {
         return root;
     }
 }
+
+class Solution2 {
+    public TreeNode reConstructBinaryTree(int[] pre, int[] in) {
+        if (pre == null || in == null) return null;
+        TreeNode root = new TreeNode(pre[0]);
+        for (int i = 0; i < in.length; i++) {
+            if (in[i] == pre[0]) {
+                root.left = reConstructBinaryTree(Arrays.copyOfRange(pre, 1, i + 1), Arrays.copyOfRange(in, 0, i));
+                root.right = reConstructBinaryTree(Arrays.copyOfRange(pre, i + 1, pre.length), Arrays.copyOfRange(in, i, in.length));
+                break;
+            }
+        }
+        return root;
+    }
+}
+
+/*
+ * 总结
+ * 1.考虑无左节点或右节点的情况；
+ * 2.对于大问题可以化成许多相同小问题的题目，可以使用递归处理。
+ */
