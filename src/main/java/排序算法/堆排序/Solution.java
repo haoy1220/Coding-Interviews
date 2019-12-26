77242204db957c4d2ed2d25248643f488318221b
hao <haoaying@outlook.com>
2019-12-26 11:00:42 +0800

    update 堆排序

--- src/main/java/排序算法/堆排序/Solution.java
+++ src/main/java/排序算法/堆排序/Solution.java
@@ -5,48 +5,46 @@ package 排序算法.堆排序;
  * @date 2019/11/23 17:44
  */
 public class Solution {
-    public void heapSort(int[] nums) {
-        for (int i = 0; i < nums.length; i++) {
-            maxHeapify(nums, nums.length - 1);
-            int temp = nums[0];
-            nums[0] = nums[nums.length - 1 - i];
-            nums[nums.length - 1 - i] = temp;
+    public void heapSort(int[] arrays) {
+        /*
+        数组堆化
+         */
+        int len = arrays.length - 1;
+        int beginIndex = (len - 1) >> 1;//第一个非叶子结点
+        for (int i = beginIndex; i >= 0; i--) {//从第一个非叶子结点开始建堆
+            maxHeapify(arrays, i, len);
+        }
+
+        /*
+        堆化数据排序
+         */
+        for (int i = len; i > 0; i--) {
+            swap(arrays, 0, i);//把最大的元素换到最后
+            maxHeapify(arrays, 0, i - 1);//重新整理交换后的根结点
         }
 
     }
 
-    public void maxHeapify(int[] nums, int size) {
-        for (int i = size - 1; i >= 0; i--) {
-            heapify(nums, i, size);
+    private void maxHeapify(int[] arrays, int index, int len) {
+        int left = (index << 1) + 1;
+        int right = left + 1;
+        int cMax = left;//默认左子结点最大
+        if (left > len) return;
+        if (right <= len && arrays[right] > arrays[left]) {//比较左右子节点
+            cMax = right;
         }
+
+        if (arrays[cMax] > arrays[index]) {//最大子结点与当前结点比较
+            swap(arrays, cMax, index);
+            maxHeapify(arrays, cMax, len);//交换后需要保持下面的结点依旧保有堆的特性
+        }
+
     }
 
-    public void heapify(int[] nums, int root, int size) {
-        if (root < size) {
-            int left = 2 * root + 1;
-            int right = 2 * root + 2;
-
-            int max = root;
-
-            if (left < size) {
-                if (nums[max] < nums[left]) {
-                    max = left;
-                }
-            }
-
-            if (right < size) {
-                if (nums[max] < nums[right]) {
-                    max = right;
-                }
-            }
-
-            if (max != root) {
-                int temp = nums[max];
-                nums[max] = nums[root];
-                nums[root] = temp;
-
-                heapify(nums, max, size);
-            }
-        }
+    private void swap(int[] arrays, int i, int j) {
+        int temp = arrays[i];
+        arrays[i] = arrays[j];
+        arrays[j] = temp;
     }
+
 }
--- src/test/java/排序算法/堆排序/SolutionTest.java
+++ src/test/java/排序算法/堆排序/SolutionTest.java
@@ -15,9 +15,10 @@ public class SolutionTest {
         int[] arrays = {6, 3, 8, 7, 5, 1, 2, 23, 4321, 432, 3,2,34234,2134,1234,5,132423, 234, 4, 2, 4, 1, 5, 2, 5};
         new Solution().heapSort(arrays);
         for (int i:
-             arrays) {
+                arrays) {
             System.out.println(i);
 
         }
     }
+
 }
